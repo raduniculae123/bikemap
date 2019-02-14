116ba5849675290c7df0271eb3f3aa32f6cf7759
radun <raduniculae@ymail.com>
2019-02-14 22:46:57 +0200

    commit

---
+++ app/src/main/java/com/bikeology/bikemaps/SplashActivity.java
@@ -0,0 +1,17 @@
+package com.bikeology.bikemaps;
+
+import android.content.Intent;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.ProgressBar;
+
+public class SplashActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_splash);
+
+    }
+}
